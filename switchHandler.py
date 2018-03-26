ac5b72160a576c5321a7f5a37d4f94250f62f0f6
b2l <b2l.powa@gmail.com>
2018-03-26 10:57:39 +0200

    Correct pin number

--- switchHandler.py
+++ switchHandler.py
@@ -1,8 +1,8 @@
 from time import sleep
 import RPi.GPIO as GPIO
 
-switchPin=4
-thermostatPin=1
+switchPin=16
+thermostatPin=12
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(switchPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
