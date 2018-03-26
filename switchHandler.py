0ac1be5733b696f45361ddd85962a773377ae987
b2l <b2l.powa@gmail.com>
2018-03-26 10:51:53 +0200

    Switch handler

--- switchHandler.py
+++ switchHandler.py
@@ -8,7 +8,10 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(switchPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def my_callback(channel):
-    val = !GPIO.input(thermostatPin)
-    GPIO.write(thermostatPin, val)
+    val = GPIO.input(thermostatPin)
+    if val is 0:
+        GPIO.write(thermostatPin, 1)
+    else:
+        GPIO.write(thermostatPin, 0)
 
 GPIO.add_event_detect(switchPinpin, GPIO.RISING, callback=my_callback, bouncetime=300)
